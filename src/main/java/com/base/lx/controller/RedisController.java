7ec1e40407f9a0728584b2dee346fa5e0213adfa
侯朋威 <houpengwei-dkh@bestpay.com.cn>
2023-01-12 10:39:55 +0800

    新增redis

---
+++ src/main/java/com/base/lx/controller/RedisController.java
@@ -0,0 +1,29 @@
+package com.base.lx.controller;
+
+import com.base.lx.util.RedisUtil;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+
+@Controller
+@RequestMapping("/redis")
+public class RedisController {
+
+    @Resource
+    private RedisUtil redisUtil;
+
+    @GetMapping("/get")
+    public void get(){
+        System.out.println(redisUtil.getString("cscs"));
+    }
+
+    @GetMapping("/put")
+    @ResponseBody
+    public void put(){
+        redisUtil.insertTime("cscs","nb",3000l);
+    }
+
+}
