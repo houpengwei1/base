f25dd930692b75528f24357f74b033f27f461d40
侯朋威 <houpengwei-dkh@bestpay.com.cn>
2023-01-12 10:12:00 +0800

    第一次提交 空框架

---
+++ src/main/java/com/base/lx/LxApplication.java
@@ -0,0 +1,13 @@
+package com.base.lx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/base/lx/LxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.base.lx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
